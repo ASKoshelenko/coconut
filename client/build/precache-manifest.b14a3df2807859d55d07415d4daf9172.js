e3402471061f3814c09bdabbe350324a194427bc
ASK <39233929+ASKoshelenko@users.noreply.github.com>
2022-08-09 10:47:33 +0300

    Add files via upload

---
+++ client/build/precache-manifest.b14a3df2807859d55d07415d4daf9172.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3eb18e15479a66a32740",
+    "url": "/static/css/main.1a3d70c2.chunk.css"
+  },
+  {
+    "revision": "3eb18e15479a66a32740",
+    "url": "/static/js/main.3eb18e15.chunk.js"
+  },
+  {
+    "revision": "84e2b47b293fa6cb0cda",
+    "url": "/static/js/1.84e2b47b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "693f9ceb1b4b7e3de3ccd3f58022746e",
+    "url": "/static/media/coco.693f9ceb.png"
+  },
+  {
+    "revision": "fe4c678f73f62ad3880485dcfb9fea94",
+    "url": "/static/media/cocobg.fe4c678f.jpg"
+  },
+  {
+    "revision": "ae1c0618e46eb08de65d317a968a9d84",
+    "url": "/index.html"
+  }
+];
